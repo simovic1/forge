e13bf89f9d23c93ce6b1522c865206ba26a7b34b
Tomislav Simovic <tomislav.simovic@luminumict.hr>
2026-06-20 07:53:51 +0200

    chore: initialize repository structure

---
+++ backend/src/test/java/com/simovic1/forge/ForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simovic1.forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
