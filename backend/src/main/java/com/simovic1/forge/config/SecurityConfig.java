a1f22f2d7410d73d3006932996d82ec380240a8d
Tomislav Simovic <tomislav.simovic@luminumict.hr>
2026-06-20 08:23:50 +0200

    style: security filter chain

--- backend/src/main/java/com/simovic1/forge/config/SecurityConfig.java
+++ backend/src/main/java/com/simovic1/forge/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/health", "/actuator/health/**").permitAll()
